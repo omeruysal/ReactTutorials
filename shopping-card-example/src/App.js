3394b5c6fcf0c05fa501904a34bb68b0b43a982d
omer <teouysal@outlook.com>
2021-06-04 09:44:10 +0300

    added shopping-card-example folder

---
+++ shopping-card-example/src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./styles.css";
+import {useInput} from './useInputs';
+
+export default function App() {
+  // const [name, setName] = useState("");
+  // const [age, setAge] = useState("");
+  // const [email, setEmail] = useState("");
+
+
+  const [inputs, setInputs] = useInput("inputs", INITIAL_STATE); //Localstorage ta saklanacak keywordu ve Localstorage bos olmasi durumuna karsilik default state'i gondeririz
+
+ 
+  return (
+    <div className="App">
+      <form>
+        <label htmlFor="name">
+        Name: <input name="name" value={inputs.name} onChange={setInputs} />
+        </label>
+        <label htmlFor="age">
+        Age: <input name="age" value={inputs.age} onChange={setInputs} />
+        </label>
+        <label htmlFor="email">
+        Email: <input name="email" value={inputs.email} onChange={setInputs} />
+        </label>
+      </form>
+    </div>
+  );
+}
